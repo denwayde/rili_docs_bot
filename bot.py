70194c1a120468b2336e163e30c8ec92d246750c
denwayde <denwayde09gmail.com>
2023-03-12 14:25:22 +0500

    add working aiogram

--- bot.py
+++ bot.py
@@ -1,25 +1,27 @@
-import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher.filters import Command
 
+from aiogram import Bot, Dispatcher, executor, types
 from config_reader import config
 
-# Включаем логирование, чтобы не пропустить важные сообщения
+API_TOKEN = config.bot_token.get_secret_value()
+
+# Configure logging
 logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(token=config.bot_token.get_secret_value())
-# Диспетчер
-dp = Dispatcher()
-#hello world
-# Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
 
-# Запуск процесса поллинга новых апдейтов
-async def main():
-    await dp.start_polling(bot)
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Здравствуйте это бот документооборота для РИЛИ он очень крутой...")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
